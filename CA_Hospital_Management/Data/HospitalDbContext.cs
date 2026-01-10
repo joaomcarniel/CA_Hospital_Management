36724676a235b2867e34b7eb06c1dcc1de12f5a8
Joao Marcos Carniel <joaomarcoscrr@gmail.com>
2026-01-10 13:30:30 +0000

    Repository, Entity Framework and Models

---
+++ CA_Hospital_Management/Data/HospitalDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CA_Hospital_Management.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA_Hospital_Management.Data
+{
+    public class HospitalDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors => Set<Doctor>();
+        public DbSet<Nurse> Nurses => Set<Nurse>();
+        public DbSet<Patient> Patients => Set<Patient>();
+        public DbSet<Consultation> Consultations => Set<Consultation>();
+        public DbSet<ConsultationNurse> ConsultationNurses => Set<ConsultationNurse>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=.;Database=HospitalManagement;Trusted_Connection=True;TrustServerCertificate=True"
+            );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doctor>().ToTable("Doctors");
+            modelBuilder.Entity<Nurse>().ToTable("Nurses");
+            modelBuilder.Entity<Patient>().ToTable("Patients");
+            modelBuilder.Entity<Consultation>().ToTable("Consultations");
+            modelBuilder.Entity<ConsultationNurse>().ToTable("ConsultationNurses");
+        }
+    }
+}
